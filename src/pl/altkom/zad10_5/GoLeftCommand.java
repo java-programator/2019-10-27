2cd93753de42a08d6ffe5c4b1eaf9e9338f2f0b2
student <student@gmail.com>
2019-10-27 09:40:31 +0100

    tttt

--- src/pl/altkom/zad10_5/GoLeftCommand.java
+++ src/pl/altkom/zad10_5/GoLeftCommand.java
@@ -1,8 +1,11 @@
 package pl.altkom.zad10_5;
 
 public class GoLeftCommand implements Command {
+    int steps;
+
+    public GoLeftCommand(int steps)
     @Override
-    public void execute() {
+    public void execute(RobotOperator operator) {
 
     }
 }
--- src/pl/altkom/zad10_5/RobotOperator.java
+++ src/pl/altkom/zad10_5/RobotOperator.java
@@ -4,6 +4,11 @@ public class RobotOperator {
     private int x;
     private int y;
 
+    RobotOperator(){
+        int x = 0;
+        int y = 0;
+    }
+
     public void doCommand(Command command) {
         command.execute();
     }
